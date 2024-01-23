57ff940bf58d569b02997c456cbdc0ff0252d3fc
hieu <nguyenxuanhieuktb10@gmail.com>
2024-01-23 14:34:41 +0700

    Add cors

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const app = express();
 const server = http.createServer(app);
 const IP = require('ip');
 const io = socketIO(server, {
-    maxHttpBufferSize: 1e7
+    maxHttpBufferSize: 1e7,
+    cors: {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
 });
+
 const SocketMsgModel = require('./msg.model.js');
 
 io.eio.pingTimeout = 120000;
@@ -53,8 +60,15 @@ io.on('connection', (socket) => {
 
 });
 
+// Route handler for GET request to the root URL ("/")
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://${IP.address()}:${PORT}`);
 });
+
+
